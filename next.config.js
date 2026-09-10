22ef455552f8a4f42490d9be95966ba15442b7f0
mustika100m <boylaksono.ungres@gmail.com>
2026-09-10 15:12:44 +0700

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    // Disable external server components restrictions if any
+  }
+}
+
+module.exports = nextConfig
